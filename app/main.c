e800732ca8398d13881ca2d0cd3490c035a68c98
Yaroslav Kadyrov <kadyrov@cs.petrsu.ru>
2023-10-31 10:21:05 +0300

    31/10

--- app/main.c
+++ app/main.c
@@ -5,10 +5,22 @@
 int main()
 {
     double *roots;
-    printf("Hello World!\n");
-    printf("%f\n", my_sqrt(9));
-    roots = realQuadraticRoots(1, -4, 4);
-    printf("%f\n", roots[0]);
-    printf("%f\n", roots[1]);
+
+    printf("Enter number\n");
+    double n = 0;
+    scanf("%lf", &n);
+    printf("SQRT %f\n", my_sqrt(n));
+    printf("Fibonachi %d\n", fibonachi(n));
+    printf("SQRT Fibonachi %f\n", my_sqrt(fibonachi(n)));
+    printf("Enter a, b, c\n");
+    double a = 0;
+    scanf("%lf", &a);
+    double b = 0;
+    scanf("%lf", &b);
+    double c = 0;
+    scanf("%lf", &c);
+    roots = realQuadraticRoots(a, b, c);
+    printf("ROOT1 %f\n", roots[0]);
+    printf("ROOT2 %f\n", roots[1]);
     free(roots);
 }
